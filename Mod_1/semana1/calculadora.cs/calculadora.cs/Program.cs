15295481249959f7c48c36eed11fb59eb8b2a934
@mariacarolinaboabaid <carolkboabaid@gmail.com>
2023-02-07 22:01:42 -0300

    Meu primeiro commit

---
+++ Mod_1/semana1/calculadora.cs/calculadora.cs/Program.cs
@@ -0,0 +1,59 @@
+﻿// Importação de biblioteca
+
+// Nome da classe
+
+// Declaração de método
+
+// Declaração de variáveis
+int opcao, numero1, numero2;
+float resultado;
+
+// Entradas
+Console.Write("Escolha uma operação:  \n\n" +
+    "1 - Soma \n" +
+    "2 - Subtração \n" +
+    "3 - Multiplicação \n" +
+    "4 - Divisão \n" +
+    "5 - Sair \n\n" +
+    "Digite a opção escolhida: ");
+
+opcao = int.Parse(Console.ReadLine());
+
+// Processamento
+switch(opcao)
+{
+    case 1:
+        Console.WriteLine("Operação somar.");
+
+        Console.Write("Digite o número 1: ");
+        numero1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o número 2: ");
+        numero2 = int.Parse(Console.ReadLine());
+
+        resultado = numero1 + numero2;
+        Console.WriteLine("Resultado: " + resultado);
+        break;
+
+    case 2:
+        Console.WriteLine("Operação subtração.");
+        break;
+
+    case 3:
+        Console.WriteLine("Operação multiplicação.");
+        break;
+
+    case 4:
+        Console.WriteLine("Operação divisão.");
+        break;
+
+    case 5:
+        Console.WriteLine("Saindo do sistema.");
+        break;
+}
+
+// Saídas
+int num1 = 10;
+float num2 = 0.9f;
+
+Console.WriteLine(num1 + num2);
---
+++ Mod_1/semana1/olaMundo.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int numeroInteiro = 10;
+long numeroMaior = 100000999L;
+float numeroQuebrado = 6.8f;
+double numeroQuebradoMaior = 101.99999999D;
+char letra = 'z';
+string texto = "Olá";
+bool varialBooleana = true;
+
+
+DateTime data = DateTime.Now; // Data até segundos
+DateTime outraData = new DateTime(2023, 01, 31);
+Console.WriteLine(data);
+Console.WriteLine(outraData);
+
+texto.ToUpper();
+
+string nomeCompleto; // Declaração de variável
+
+Console.Write("Digite o seu nome: ");
+nomeCompleto = Console.ReadLine();
+Console.WriteLine("O seu nome é " + nomeCompleto);
