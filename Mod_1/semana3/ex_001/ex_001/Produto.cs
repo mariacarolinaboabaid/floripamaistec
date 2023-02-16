bfae0c902b338e9eef3f8bbcfde7c2d800f36ccc
@mariacarolinaboabaid <carolkboabaid@gmail.com>
2023-02-16 15:31:19 -0300

    Inclusão do  [M1-Sem03-Ex001]

---
+++ Mod_1/semana3/ex_001/ex_001/Produto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+// Escopo da classe 
+namespace ex_001
+{
+	// Criando a Classe
+	public class Produto
+	{
+
+		// Atributos
+		public string nome;
+		public double preco;
+		public int quantidade;
+
+        // Construtor c/ argumentos
+        public Produto (string nome, double preco, int quantidade)
+		{
+			this.nome = nome;
+			this.preco = preco;
+			this.quantidade = quantidade;
+		}
+
+		// Construtor vazio
+		public Produto()
+        {
+            nome = "Sem nome";
+            preco = 0;
+            quantidade = 0;
+        }
+    }
+
+
+	
+}
+
---
+++ Mod_1/semana3/ex_001/ex_001/Program.cs
@@ -0,0 +1,18 @@
+﻿using ex_001;
+
+// Instanciando a Classe com o construtor c/ argumentos
+Produto produto_01 = new Produto("Garrafa de água", 30.50d, 3);
+
+
+// Instanciando a Classe com o construtor vazio
+Produto produto_02 = new Produto();
+
+Console.WriteLine($"1º Produto \n" +
+    $"Nome: {produto_01.nome} \n" +
+    $"Preço: R$ {produto_01.preco} \n" +
+    $"Quantidade: {produto_01.quantidade} \n" +
+    "-----------------------------------------------------------------------\n" +
+    $"2º Produto: \n" +
+    $"Nome: {produto_02.nome} \n" +
+    $"Preço: R$ {produto_02.preco} \n" +
+    $"Quantidade: {produto_02.quantidade}");
