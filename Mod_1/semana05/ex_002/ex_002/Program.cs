dc3ed3e1af7a56866c3aff591ab3d5d084e3a9ac
@mariacarolinaboabaid <carolkboabaid@gmail.com>
2023-03-06 13:22:56 -0300

    Exercícios [M1-Sem5]

---
+++ Mod_1/semana05/ex_002/ex_002/Program.cs
@@ -0,0 +1,33 @@
+﻿FileInfo arquivo = null;
+
+// Tratamento de exceção
+try
+{
+    
+    // Pedindo ao Usuário o nome do arquivo
+    Console.Write("Digite o nome do arquivo que você irá criar:  ");
+    string nomeArquivo = Console.ReadLine();
+
+    // Nova instanciação do FileInfo (crie esse arquivo)
+    arquivo = new FileInfo(nomeArquivo);
+
+    // Adicione o texto dentro do arquivo 
+    using (StreamWriter gravadorFluxo = File.AppendText(nomeArquivo))
+    {
+        gravadorFluxo.WriteLine("Texto de dentro do arquivo");
+    }
+}
+
+// Exceção genérica
+catch (Exception ex)
+{
+    Console.WriteLine($"Ocorreu o erro: {ex.Message}");
+}
+
+finally
+{
+    Console.WriteLine("Encerrando programa.");
+    arquivo = null;
+}
+
+
