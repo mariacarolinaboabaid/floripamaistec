cb4e9ea026c2c2d9478bfea78f8a6c6ccbe01b6b
@mariacarolinaboabaid <carolkboabaid@gmail.com>
2023-03-06 13:16:19 -0300

    Exercícios [M1-Sem5]

---
+++ Mod_1/semana05/ex_001/ex_001/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+List<decimal> numeros = new List<decimal> { };
+// Procedendo a divisão
+try
+{
+    
+    for (int i = 1; i <= 2; i++)
+    {
+        Console.Write($"Digite o {i}º número: ");
+        decimal numero = decimal.Parse(Console.ReadLine());
+        numeros.Add(numero);
+    }
+    decimal resultado = numeros[0] / numeros[1];
+    Console.WriteLine($"O resultado da divisão do número {numeros[0]} pelo número {numeros[1]} é {resultado}.");
+}
+// Exceção divisão por zero
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine($"Error: o dividendo não pode ser 0. \n" +
+        $"{ex.Message}");
+}
+// Exceção tipo de formato
+catch (FormatException ex)
+{
+    Console.WriteLine($"Error: insira apenas dígitos numéricos.\n" +
+        $"{ex.Message}");
+}
+// Exceção genérica 
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: ocorreu um erro. \n" +
+        $"{ex.Message}");
+}
+// Zerando
+finally
+{
+    numeros[0] = 0;
+    numeros[1] = 0;
+}
