b630859e427f001e27b9f18521444eedee6c629c
@mariacarolinaboabaid <carolkboabaid@gmail.com>
2023-03-23 18:37:05 -0300

    Atualização Exercícios

---
+++ Mod_1/semana08/ex_007/Escola/Escola/Controllers/AlunosControllers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Escola.Models;
+using Escola.Repositories;
+
+
+namespace Escola.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class AlunosControllers : ControllerBase
+	{
+
+		
+	}
+}
+
