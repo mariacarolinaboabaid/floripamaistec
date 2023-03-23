d1380f04be5185b9db79974addf3e0d4e6577ec5
@mariacarolinaboabaid <carolkboabaid@gmail.com>
2023-03-23 19:10:14 -0300

    Atualização Exercícios

---
+++ Mod_1/semana08/ex_008/Escola/Escola/Controllers/AlunosController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Escola.Models;
+using Escola.Repositories;
+
+namespace Escola.Controllers
+{
+
+	[ApiController]
+	[Route("[controller]")]
+	public class AlunosController : ControllerBase
+	{
+		// Método para listar todos os alunos
+		[HttpGet]
+		[Route("listar-alunos")]
+		public IActionResult ListarAlunos(string? nome)
+		{
+			var repository = new AlunosRepository();
+			var alunos = repository.ListarAlunosRepository(nome);
+            return Ok(alunos);
+		}
+	}
+}
+
---
+++ Mod_1/semana08/ex_008/Escola/Escola/Repositories/AlunosRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Escola.Models;
+
+namespace Escola.Repositories
+{
+    public class AlunosRepository
+    {
+        // Lista estática para os dados testes
+        private static List<AlunoModel> lista = new List<AlunoModel>()
+            {
+             new AlunoModel {Id =  1, DataDeInclusao = new DateTime(2022, 03, 23), DataDeAlteracao = new DateTime(2023, 03, 23),
+                 DataNascimento = new DateTime(1997, 10, 14), Nome = "Betânia Maria" },
+             new AlunoModel {Id =  2, DataDeInclusao = new DateTime(2022, 04, 23), DataDeAlteracao = new DateTime(2023, 03, 23),
+                 DataNascimento = new DateTime(2000, 05, 05), Nome = "Marcelo Luterino"},
+             new AlunoModel {Id =  3, DataDeInclusao = new DateTime(2022, 08, 18), DataDeAlteracao = new DateTime(2023, 03, 23),
+                 DataNascimento = new DateTime(1994, 02, 18), Nome = "Maria Regina Berger" },
+             new AlunoModel {Id =  4, DataDeInclusao = new DateTime(2020, 10, 27), DataDeAlteracao = new DateTime(2023, 03, 23),
+                 DataNascimento = new DateTime(2005, 07, 02), Nome = "Bernardo Rosa" },
+            };
+
+        // Método para listar os alunos com opção de passar 'nome' como parâmetro
+        public List<AlunoModel> ListarAlunosRepository(string filtroNome)
+        {
+            if (string.IsNullOrEmpty(filtroNome))
+            {
+                return lista;
+            }
+            else
+            {
+                return lista.Where(x => x.Nome.ToUpper().Contains(filtroNome.ToUpper())).ToList();
+            }
+        }
+    }
+}
