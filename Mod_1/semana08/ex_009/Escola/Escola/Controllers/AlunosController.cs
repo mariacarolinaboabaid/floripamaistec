6fc8725ab06f3ddc5e6f2d11aec7dba5bbbeb7c2
@mariacarolinaboabaid <carolkboabaid@gmail.com>
2023-03-23 19:16:20 -0300

    Atualização Exercícios

---
+++ Mod_1/semana08/ex_009/Escola/Escola/Controllers/AlunosController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Escola.Models;
+using Escola.Repositories;
+
+namespace Escola.Controllers
+{
+
+	[ApiController]
+	[Route("[controller]")]
+	public class AlunosController : ControllerBase
+	{
+		// Método para listar todos os alunos
+		[HttpGet]
+		[Route("listar-alunos")]
+		public IActionResult ListarAlunos(string? nome)
+		{
+			var repository = new AlunosRepository();
+			var alunos = repository.ListarAlunosRepository(nome);
+            return Ok(alunos);
+		}
+
+        // Método para obter aluno por ID
+        [HttpGet]
+        [Route("obter-aluno-por-{id}")]
+        public IActionResult ObterPorID(int id)
+        {
+            var repository = new AlunosRepository();
+            var aluno = repository.ObterAlunoPorID(id);
+            // Resposta Error 400
+            if (id <= 0)
+            {
+                return BadRequest("ID deve ser maior que 0.");
+            }
+            // Caso o ID não seja encontrado
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+            // Resposta Success 200
+            return Ok(aluno);
+        }
+    }
+}
+
